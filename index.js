1ff250459c6d9f49d7c630c88b390900fac27196
Efrata Habte <efratahabte1213@gmail.com>
2025-12-10 23:19:27 +0300

    Refactor button event listeners to use querySelectorAll for number and operator buttons; streamline code for better maintainability

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-const one= document.querySelector('#one');
-const two= document.querySelector('#two');
-const three= document.querySelector('#three');
-const four= document.querySelector('#four');
-const five= document.querySelector('#five');
-const six= document.querySelector('#six');
-const seven= document.querySelector('#seven');
-const eight= document.querySelector('#eight');
-const nine= document.querySelector('#nine');
-const zero= document.querySelector('#zero');
+const numberButton= document.querySelectorAll(".number")
+const operatorButton= document.querySelectorAll(".operator")
 
-const plus= document.querySelector('#plus');
-const minus= document.querySelector('#minus');
-const multiply= document.querySelector('#multiply');
-const divide= document.querySelector('#divide');
 const equal= document.querySelector('#equal');
 const clear= document.querySelector('#clear');
 const dot= document.querySelector('#dot');
-const modulo= document.querySelector('#modulo');
 const display=document.querySelector('#result');
 
 let firstNum=''
@@ -24,114 +11,32 @@ let secondNum=''
 let operator=''
 let result=''
 
-one.addEventListener('click',()=>{
+function appendNumber(num){
     if (operator===""){
-        firstNum+=one.textContent;
-        display.value+=one.textContent;
+        firstNum+=num;
+        display.value+=num;
     }
     else{
-        secondNum+=one.textContent;
-        display.value+=secondNum;
+        secondNum+=num;
+        display.value+=num;
     }
+}
+
+function appendOperator(oper){
+    operator=oper;
+    display.value+=oper;
+}
+
+numberButton.forEach(btn=>{
+    btn.addEventListener('click',()=>{
+        appendNumber(btn.textContent);
+    })
 })
 
-two.addEventListener('click',()=>{
-    if (operator===""){
-        firstNum+=two.textContent;
-        display.value+=two.textContent;
-    }
-    else{
-        secondNum+=two.textContent;
-        display.value+=two.textContent;
-    }
-})
-
-three.addEventListener('click',()=>{
-    if (operator===""){
-        firstNum+=three.textContent;
-        display.value+=three.textContent;
-    }
-    else{
-        secondNum+=three.textContent;
-        display.value+=three.textContent;
-    }
-})
-
-four.addEventListener('click',()=>{
-    if (operator===""){
-        firstNum+=four.textContent;
-        display.value+=four.textContent;
-    }
-    else{
-        secondNum+=four.textContent;
-        display.value+=four.textContent;
-    }
-})
-
-five.addEventListener('click',()=>{
-    if (operator===""){
-        firstNum+=five.textContent;
-        display.value+=five.textContent;
-    }
-    else{
-        secondNum+=five.textContent;
-        display.value+=five.textContent;
-    }
-})
-
-six.addEventListener('click',()=>{
-    if (operator===""){
-        firstNum+=six.textContent;
-        display.value+=six.textContent;
-    }
-    else{
-        secondNum+=six.textContent;
-        display.value+=six.textContent;
-    }
-})
-
-seven.addEventListener('click',()=>{
-    if (operator===""){
-        firstNum+=seven.textContent;
-        display.value+=seven.textContent;
-    }
-    else{
-        secondNum+=seven.textContent;
-        display.value+=seven.textContent;
-    }
-})
-
-eight.addEventListener('click',()=>{
-    if (operator===""){
-        firstNum+=eight.textContent;
-        display.value+=eight.textContent;
-    }
-    else{
-        secondNum+=eight.textContent;
-        display.value+=eight.textContent;
-    }
-})
-
-nine.addEventListener('click',()=>{
-    if (operator===""){
-        firstNum+=nine.textContent;
-        display.value+=nine.textContent;
-    }
-    else{
-        secondNum+=nine.textContent;
-        display.value+=nine.textContent;
-    }
-})
-
-zero.addEventListener('click',()=>{
-    if (operator===""){
-        firstNum+=zero.textContent;
-        display.value+=zero.textContent;
-    }
-    else{
-        secondNum+=zero.textContent;
-        display.value+=zero.textContent;
-    }
+operatorButton.forEach(op=>{
+    op.addEventListener('click',()=>{
+        appendOperator(op.textContent);
+    })
 })
 
 dot.addEventListener('click',()=>{
@@ -143,36 +48,6 @@ dot.addEventListener('click',()=>{
         secondNum+=dot.textContent;
         display.value+=dot.textContent;
     }
-})
-
-plus.addEventListener('click',()=>{
-    operator=plus.textContent;
-    display.value+=operator;
-    
-})
-
-minus.addEventListener('click',()=>{
-    operator=minus.textContent;
-    display.value+=operator;
-    
-})
-
-multiply.addEventListener('click',()=>{
-    operator=multiply.textContent;
-    display.value+=operator;
-    
-})
-
-divide.addEventListener('click',()=>{
-    operator=divide.textContent;
-    display.value+=operator;
-    
-})
-
-modulo.addEventListener('click',()=>{
-    operator=modulo.textContent;
-    display.value+=operator;
-    
 })
 
 equal.addEventListener('click',()=>{
@@ -193,6 +68,8 @@ equal.addEventListener('click',()=>{
     }
 
     display.value=result;
+    firstNum=""
+    secondNum=""
     firstNum=result;
     operator="";
 })
@@ -205,5 +82,14 @@ clear.addEventListener('click',()=>{
     result="";
 })
 
+del.addEventListener('click',()=>{
+    display.value=display.value.slice(0,-1);
+    if (operator===""){
+        firstNum=firstNum.slice(0,-1);
+    }
+    else{
+        secondNum=secondNum.slice(0,-1);
+    }
+})
 
 
